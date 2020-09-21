3d732b16c3964cfe161d05f2ec840f06212fd30c
george-simon <george-simon@users.noreply.github.com>
2020-09-20 19:54:57 -0700

    flask build out

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,30 @@
+# Dependencies
+from bs4 import BeautifulSoup
+from splinter import Browser
+import requests
+import pymongo
+import cssutils
+import pandas as pd
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "C:/Users/simo8/Documents_PC/bootcamp/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    listings = {}
+
+    url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    listings["headline"] = soup.find("a", class_="result-title").get_text()
+    listings["price"] = soup.find("span", class_="result-price").get_text()
+    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+
+    return listings
+ 
